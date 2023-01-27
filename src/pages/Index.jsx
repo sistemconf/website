1e6cbe554bade54485e0bf0d104c9b7c95fd7022
Rían Errity <rian@paradaux.io>
2023-01-27 23:15:42 +0100

    Setup react router

---
+++ src/pages/Index.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/static/Navbar"
+import "./index.scss"
+
+export default function Index() {
+    return (
+        <div className="index">
+            <Navbar/>
+        </div>
+    )
+}
