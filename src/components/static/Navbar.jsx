f65b387bb3395d27509467ce3b95e246448450c1
Rían Errity <rian@paradaux.io>
2023-02-02 13:41:38 +0100

    WIP Hero and redesigned nav

--- src/components/static/Navbar.jsx
+++ src/components/static/Navbar.jsx
@@ -14,18 +14,16 @@ export default function Navbar() {
     
     console.log(hamburgerOpen);
     return (
-        <div className="navbar">
-            <img src="https://media.discordapp.net/attachments/1030196142471905350/1067805289371418655/Screenshot_2023-01-25_at_14.48.17.png"/>
-            <nav className="navbar-links">
-                <NavbarLink href="" title="Home"/>
-                {/* <NavbarLink href="" title="Speakers"/> */}
-                {/* <NavbarLink href="" title="Schedule"/> */}
-                <NavbarLink href="" title="Team"/>
-                <NavbarLink href="" title="Code of Conduct"/>
-            </nav>
-            <button className={"hamburger" + (hamburgerOpen ? " is-active" : "")} onClick={toggleHamburger}>
-                <div className="bar"/>
-            </button>
-        </div>
+        <header>
+            <div className="container">
+                <h2><span>SISTEM</span>2023</h2>
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Team</a>
+                    <a href="#">Code of Contact</a>
+                    <a href="#">Buy Tickets</a>
+                </nav>
+            </div>
+        </header>
     )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,6 +5,19 @@ export default function Index() {
     return (
         <div className="index">
             <Navbar/>
+            <main>
+                <section class="banner">
+                    <div className="container">
+                        <h1>
+                            A <span>Student</span> Tech<br class="hide-mobile"/> & Enterprise Conference.
+                        </h1>
+                            <h3>March 11<sup>th</sup> 2023, Dublin City University.</h3>
+                            <a href="#" class="button">Buy Tickets</a>
+                            <a href="#" class="button secondary">Schedule</a>
+
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
