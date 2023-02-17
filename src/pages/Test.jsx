4eb400e35fd7ac526bf0800b056db0402004bd41
Rían Errity <rian@paradaux.io>
2023-02-17 16:38:34 +0100

    Add icons, add Andreea

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,20 +1,11 @@
-import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
 import "./Test.scss"
 
-const position = [51.505, -0.09]
+
 
 export default function Test() {
     return (
         <div>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <div></div>
-    </Marker>
-  </MapContainer>   
+          <img src={rianImg} className="sistem-gradient"/>
         </div>
     )
 }
