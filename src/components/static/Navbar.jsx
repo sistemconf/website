47a0c11f74aacab90e1fce9eeaced203cc68da3d
Rían Errity <rian@paradaux.io>
2023-02-21 13:31:51 +0100

    Mobile nav finished for now

--- src/components/static/Navbar.jsx
+++ src/components/static/Navbar.jsx
@@ -13,19 +13,28 @@ export default function Navbar({hasBg}) {
     const toggleHamburger = () => setHamburgerOpen((prev) => !prev);
 
     return (
-        <header className={hasBg ? "is-scrolling" : ""}>
-            <div className="container">
-                <h2><span>SISTEM</span>2023</h2>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/team">Team</a>
-                    <a href="/code-of-conduct">Code of Contact</a>
-                    <a href="/#tickets" className="button">Buy Tickets</a>
+        <>
+            <header className={hasBg ? "is-scrolling" : ""}>
+                <div className="container">
+                    <h2><span>SISTEM</span>2023</h2>
+                    <nav className="desktop-nav">
+                        <a href="/">Home</a>
+                        <a href="/team">Team</a>
+                        <a href="/code-of-conduct">Code of Contact</a>
+                        <a href="/#tickets" className="button">Buy Tickets</a>
+                    </nav>
                     <button className={"hamburger" + (hamburgerOpen ? " is-active" : "")} onClick={toggleHamburger}>
                         <div className="bar"/>
                     </button>
-                </nav>
-            </div>
-        </header>
+                </div>
+            </header>
+            <nav className={"mobile-nav" + (hamburgerOpen ? " is-active" : "")}>
+                <a href="/">Home</a>
+                <a href="/team">Team</a>
+                <a href="/code-of-conduct">Code of Contact</a>
+                <a href="/#tickets" className="button">Buy Tickets</a>
+            </nav>
+        </>
+
     )
 }
