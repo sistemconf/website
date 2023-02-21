da5f0c1dcd6e7f602456b2e247e29602f7d777ca
Rían Errity <rian@paradaux.io>
2023-02-21 13:44:58 +0100

    Fix footer on mobile (and revise on desktop)

--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -4,33 +4,28 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="container">
-            <div className="footer-content">
-                <h2>
-                    <b>SISTEM<span>2023</span></b>
-                </h2>
-            </div>
-
-            <div className="footer-links">
-                <div className="footer-credit">
-                    <p> Annual event hosted by Intersocs and volunteers like you! <br/>An unincorperated association of Computer Science Societies in Ireland <br/> Website built by <a href="https://paradaux.io">Rían Errity</a>. Source available on <a href="https://github.com/sistemconf/website">GitHub</a> </p>
+                <div className="footer-content">
+                    <h2>
+                        <b>SISTEM<span>2023</span></b>
+                    </h2>
                 </div>
 
-                <div className="footer-page-links">
-                    <a href="#ß">Discord</a>
-                    <a href="https://www.instagram.com/sistem_conf/">Instagram</a>
-                    <a href="https://twitter.com/sistem_conf">Twitter</a>
-                    <a href="https://www.linkedin.com/company/sistemconf/">LinkedIn</a>
-                </div>
+                <div className="footer-links">
+                    <div className="footer-credit">
+                        <p> 
+                            Annual event hosted by Intersocs and volunteers like you! <br className="mobile-disabled"/> 
+                            An unincorperated association of Computer Science Societies in Ireland <br className="mobile-disabled"/> 
+                            Website built by <a href="https://paradaux.io">Rían Errity</a>. <br className="mobile-disabled"/>  Source available on <a href="https://github.com/sistemconf/website">GitHub</a> 
+                        </p>
+                    </div>
 
-                <div className="footer-socials">
-                    <a href="https://instagram.com/tcdtrinder">
-                        <i className="fa-brands fa-instagram"></i>
-                    </a>
-                    <a href="https://twitter.com/tcdtrinder">
-                        <i className="fa-brands fa-twitter"></i>
-                    </a>
+                    <div className="footer-page-links">
+                        <a href="https://discord.gg/US8fhv8G7d">Discord</a>
+                        <a href="https://www.instagram.com/sistem_conf/">Instagram</a>
+                        <a href="https://twitter.com/sistem_conf">Twitter</a>
+                        <a href="https://www.linkedin.com/company/sistemconf/">LinkedIn</a>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     )
