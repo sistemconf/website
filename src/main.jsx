511324e3f85548b6d1b6f5b7d3eebef43056365f
Gary Moore <madmangaz@gmail.com>
2023-03-02 00:33:09 +0000

    fix: add prettier config and dev dep to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './main.scss';
-import 'leaflet/dist/leaflet.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Index from './pages/Index';
-import Test from './pages/Test';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.scss";
+import "leaflet/dist/leaflet.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./pages/Index";
+import Test from "./pages/Test";
 
-import markerIcon from '../node_modules/leaflet/dist/images/marker-icon.png';
-import Team from './pages/Team';
-import CodeOfConduct from './pages/CodeOfConduct';
-import Speakers from './pages/Speakers';
-import speakers from './data/speakers';
+import markerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
+import Team from "./pages/Team";
+import CodeOfConduct from "./pages/CodeOfConduct";
+import Speakers from "./pages/Speakers";
+import speakers from "./data/speakers";
 
 L.Marker.prototype.setIcon(
   L.icon({
@@ -20,32 +20,28 @@ L.Marker.prototype.setIcon(
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Index />,
   },
   {
-    path: '/team',
+    path: "/team",
     element: <Team />,
   },
   {
-    path: '/code-of-conduct',
+    path: "/code-of-conduct",
     element: <CodeOfConduct />,
   },
   {
-    path: '/speakers',
-    element: (
-      <Speakers
-        speakers={speakers}
-      />
-    ),
+    path: "/speakers",
+    element: <Speakers speakers={speakers} />,
   },
   {
-    path: '/test',
+    path: "/test",
     element: <Test />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
