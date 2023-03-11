c7def6d06b1faab855f4cc6925ef5a1e5f32d4b3
Nicole <nicolemccabechu@gmail.com>
2023-03-11 08:28:42 +0000

    Fix schedule

--- src/data/scheduleData.js
+++ src/data/scheduleData.js
@@ -2,18 +2,18 @@ const data = [
     {
         time: '10:00 AM - 11:00 AM',
         talk1: 'MasterCard Opening Talk',
-        room1: 'Main Stage',
+        room1: 'Moore Auditorium',
 
     },     
     {
         time: '11:00 AM - 11:45 AM',
         talk1: 'What I Have Learned Building Local and Virtual AWS Communities',
-        room1: 'Main Stage',
+        room1: 'Moore Auditorium',
     },
     {
         time: '11:45 AM - 12:30 PM',
         talk1: 'Practical Machine Learning In Cybersecurity',
-        room1: 'Main Stage',
+        room1: 'Moore Auditorium',
         talk2: 'Diversity and Inclusion Panel',
         room2: 'Panel Room',
     },
@@ -24,7 +24,7 @@ const data = [
     {
         time: '13:00 PM - 13:45 PM',
         talk1: 'SRE: The Next Great Frontier',
-        room1: 'Main Stage',
+        room1: 'Moore Auditorium',
         talk2: 'Mastercard Lightning Talks',
         room2: 'Panel Room'
     },
@@ -36,27 +36,28 @@ const data = [
     {
         time: '14:30 PM - 15:15 PM',
         talk1: 'Get in the Robot, Shinji - The Pros and Cons of Getting into Security',
-        room1: 'Main Stage',
+        room1: 'Moore Auditorium',
         talk2: 'Gaming Panel',
         room2: 'Panel Room'
     },
     {
         time: '15:45 PM - 16:15 PM',
-        talk1: 'R3 Talk',
-        room1: 'Main Stage',
+        talk1: 'Compiling and Building Your Future',
+        room1: 'Moore Auditorium',
         talk2: 'Student Lightning Talks',
         room2: 'Panel Room'
     },
     {
         time: '16:15 PM - 17:00 PM',
         talk1: 'MasterCard Intern Panel',
-        room1: 'Main Stage',
+        room1: 'Moore Auditorium',
         talk2: 'Student Lightning Talks',
         room2: 'Panel Room'
     },
     {
         time: '17:00 PM',
         talk1: 'SISTEM Afterparty',
+        room1: 'UCD Village',
     },
 ]
 
